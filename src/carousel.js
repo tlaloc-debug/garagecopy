2a3b39da0d166cec0b03004f68f7f01874cfea31
tlaloc_debug <pelondinho@hotmail.com>
2021-05-08 13:35:03 -0400

    add css file for alice-carousel

--- src/carousel.js
+++ src/carousel.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import AliceCarousel from 'react-alice-carousel';
-import "react-alice-carousel/lib/alice-carousel.css";
+import "./alice-carousel.css";
 import "./index.css"
 
 function Carousel() {
